ea5a9bffaf5c5656498b3d2191caa08e5d32871d
Meryembentalebeljadil <meryembentaleb11@gmail.com>
2021-04-07 09:28:08 +0200

    js-exo5-part2

---
+++ main.js
@@ -0,0 +1,11 @@
+function setNewImage(){
+    document.getElementById('img1').src ="/home/bentaleb/Desktop/exercice/JavaScript/part 2/exo5-js/image/images (4).jpeg";
+} 
+function setImage(){
+    document.getElementById('img2').src="/home/bentaleb/Desktop/exercice/JavaScript/part 2/exo5-js/image/images (3).jpeg"
+
+}
+function Image(){
+    document.getElementById('img3').src="/home/bentaleb/Desktop/exercice/JavaScript/part 2/exo5-js/image/download.jpeg"
+}
+
